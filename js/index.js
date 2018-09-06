c90e16146ce251a40546fb700da15040b2700aec
YangXianSongDongHui <14729626293@163.com>
2018-09-06 21:45:00 +0800

    显示和隐藏记录日期

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ let app = new Vue({
         new_item: '', //表格新行
         show: false,
         add: false,
+        date_show: false,
         vol_select: [
             50, 100, 250, 1, 25, 10
         ],
@@ -979,6 +980,9 @@ let app = new Vue({
 
     },
     methods: {
+        date_(){
+            this.date_show = !this.date_show;
+        },
         get_list() {
             /**
              * 从标准系列表格中获取数据，存入数据组中，同时存入localStorage中。
