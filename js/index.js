657fa68081e1650cd962eba552102f4e1b697393
YangXianSongDongHui <14729626293@163.com>
2018-09-06 21:59:18 +0800

    提供打印保存功能

--- js/index.js
+++ js/index.js
@@ -980,6 +980,19 @@ let app = new Vue({
 
     },
     methods: {
+    // 打印
+     printContent(e){
+    let subOutputRankPrint = document.getElementById('subOutputRank-print');
+    console.log(subOutputRankPrint.innerHTML);
+    let newContent =subOutputRankPrint.innerHTML;
+    let oldContent = document.body.innerHTML;
+    document.body.innerHTML = newContent;
+    window.print();
+    window.location.reload();
+    document.body.innerHTML = oldContent;
+    return false;
+    },
+
         date_(){
             this.date_show = !this.date_show;
         },
