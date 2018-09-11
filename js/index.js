85244eb924b9246927d523e44869b78897f6663f
SongDongHui <14729626293@163.com>
2018-09-11 17:52:03 +0800

    逻辑混乱，有待梳理

--- js/index.js
+++ js/index.js
@@ -1356,9 +1356,9 @@ let app = new Vue({
              * */
             var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
             var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-            var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-            var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-            var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+            // var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+            // var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+            // var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
             // 拼接
             return year + "-" + month + "-" + day;
         }
--- sample.js
+++ sample.js
@@ -1,43 +1,99 @@
-let root = new Vue({
-    el: "#app",
-    data: {
-        items: [],
-        item: {
-            judgment: '',
-            name: '',
-            range: [0, 0.1],
-            result: 0,
-            id: 0,
-        }
-    },
-    methods: {
-        get_storage() {
-            let reg_std = new RegExp("[\\u4E00-\\u9FFF]+", "g");
-            let reg_result = new RegExp("[\\u4E00-\\u9FFF]+[1-9999][报告结果]", "g")
-            let names = [];
-            for (let i = 0; i < localStorage.length; i++) {
-                let get_key = localStorage.key(i);
-                this.items.push(this.item);
-                if (reg_std.test(get_key)) {
-                    this.items[i].name = get_key;
-                    names.push(get_key);
+let sample = new Vue({
+            el: "#app",
+            data: {
+                items: [],
+                item: {
+                    judgment: '',
+                    name: '',
+                    range: [0, 0.1],
+                    result: 0,
+                    id: 0,
                 }
-                if (reg_result.test(get_key)) {
-                    if (get_key.search(names[i])) {
-                        let number = get_key.replace(/^[^\d]*(\d+)[^\d]*$/, "$1");
-                        this.items[i].id = number;
-                        this.items[i].result = localStorage.getItem(get_key);
+            },
+            computed: {
+                report_number(){
+                    let arr = 0;
+                    let reg_end = new RegExp("end$","g");
+                    for(let i=0; i<localStorage.length; i++){
+                        let x = localStorage.key(i);
+                        if(reg_end.test(x)){
+                            if(localStorage.getItem(x)>arr){
+                                arr = localStorage.getItem(x);
+                            }
+                        }
                     }
+                    return arr;
+                },
+            },
+            methods: {
+                sort_obj(arr) {
+                    let new_arr = [];
+                    for (let n = 1; n < this.report_number; n++) {
+                        let new_a = [];
+                        for (let i = 0; i < arr.length; i++) {
+                            let a = arr[i].replace(/^[^\d]*(\d+)[^\d]*$/, "$1");
+                            if (a == n) {
+                                new_a.push(arr[i]);
+                            }
+                        }
+                        new_arr.push(new_a);
+                        new_a = [];
+                    }
+                    return new_arr;
+                },
+                get_storage() {
+                    // reg_std正则表达式筛选字符串的前面所有汉字
+                    let reg_std = new RegExp("^[\u4e00-\u9fa5]+$", "g");
+                    let reg_result = new RegExp("^[\u4e00-\u9fa5]+[0-9]+报告结果$", "g")
+                    let names = []
+                    let names_ids = []
+                    // let ids = []
+                    for (let i = 0; i < localStorage.length; i++) {
+                        var get_key = localStorage.key(i);
+                        if (reg_std.test(get_key)) {
+                            this.item.name = get_key;
+                            names.push(get_key);
+                        }
+                        if (reg_result.test(get_key)) {
+                            names_ids.push(get_key);
+                        }
+                    }
+                    // names_ids.sort(function(a,b){
+                    //     a = a.replace(/^[^\d]*(\d+)[^\d]*$/, "$1");
+                    //     b = b.replace(/^[^\d]*(\d+)[^\d]*$/, "$1");
+                    //     return a < b;
+                    // });
+                    let sorted_arr = this.sort_obj(names_ids);
+                    for (let i = 0; i < sorted_arr.length; i++) {
+                        for (let n = 0; n < sorted_arr[i].length; n++) {
+                            let v = localStorage.getItem(sorted_arr[i][n])
+                            value = eval(JSON.parse(v));
+                            this.item.id = v.id;
+                            this.item.result = v.result
+                            this.item.name = sorted_arr[i][n].replace(/^[\u4e00-\u9fa5]*(\u4e00-\u9fa5)[\u4e00-\u9fa5]*$/, "$1")
+                            if (this.item.result > this.item.range[0] && this.itme.result < this.item.range[1]) {
+                                this.item.judgment = "合格";
+                            } else {
+                                this.item.judgment = "不合格";
+                            }
+                        }
+                    this.items.push(this.item);
+                    this.item = {
+                        judgment: '',
+                        name: '',
+                        range: [0, 0.1],
+                        result: 0,
+                        id: 0,
+                    }
+                    }
+                    // for (let i = 0; i < names_ids.length; i++) {
+                    //     let name = names_ids[i].replace(/^[\u4e00-\u9fa5]*(\u4e00-\u9fa5)[\u4e00-\u9fa5]*$/, "$1")
+                    //     let id = names_ids[i].replace(/^[^\d]*(\d+)[^\d]*$/, "$1");
+                    //     names_ids.name = name;
+                    //     this.item.id = id;
+                    //     names_ids.push(get_key);
+                    //     this.item.result = localStorage.getItem(get_key);
+                    // }
                 }
             }
-            for (let n = 0; n < names.length; n++) {
-                if (this.items[n].result > this.items[n].range[1] || this.items[n].result < this.items[n].range[0]) {
-                    this.items[n].judgment = "不合格";
-                } else {
-                    this.items[n].judgment = "合格";
-                }
-            }
-
-        }
-    },
 })
